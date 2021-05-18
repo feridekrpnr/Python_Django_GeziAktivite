65e1a45b79ced58e0353e4a0a4989760394a000c
feridekrpnr <74022995+feridekrpnr@users.noreply.github.com>
2021-05-18 03:11:14 +0300

    Setting ayarları oluşturuldu.

--- home/views.py
+++ home/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from home.models import Setting
+
+
 def index(request):
-    text = "Merhaba Django"
-    context = {'text': text}
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
     return render(request, 'index.html', context)
